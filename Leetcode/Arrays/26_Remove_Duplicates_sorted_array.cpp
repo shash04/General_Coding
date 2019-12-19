7a2968997014ac714d40e504817ca4dfd7548469
Shashank Phalke <shashankphalke@gatech.edu>
2019-12-18 19:12:02 -0800

    Update 26_Remove_Duplicates_sorted_array.cpp

--- Leetcode/Arrays/26_Remove_Duplicates_sorted_array.cpp
+++ Leetcode/Arrays/26_Remove_Duplicates_sorted_array.cpp
@@ -21,3 +21,24 @@ public:
         return nums.size() - s;
     }
 };
+
+// Solution 2:
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) 
+    {
+        if (nums.size() == 0) return 0;
+        
+        int index = 0;
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] > nums[index])
+            {
+                if (i > index + 1)
+                    nums[index + 1] = nums[i];
+                index++;
+            }
+        }
+        return index + 1;
+    }
+};
