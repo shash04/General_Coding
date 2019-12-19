344abedaac10e20808e4b7fcce91ad9f3e244d57
Shashank Phalke <shashankphalke@gatech.edu>
2019-12-18 19:12:56 -0800

    Update 27_Remove_element.cpp

--- Leetcode/Arrays/27_Remove_element.cpp
+++ Leetcode/Arrays/27_Remove_element.cpp
@@ -21,3 +21,17 @@ public:
         return nums.size()-s;
     }
 };
+
+// Solution 2:
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int start = 0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i] != val)
+                nums[start++] = nums[i];
+        }
+        return start;
+    }
+};
