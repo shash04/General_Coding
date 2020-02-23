3bb0bc214162c87766a873b37817319b01836b04
Shashank Phalke <shashankphalke@gatech.edu>
2020-02-22 20:35:02 -0800

    Update 1361_Validate_Binary_tree_nodes.cpp

--- Leetcode/Trees/1361_Validate_Binary_tree_nodes.cpp
+++ Leetcode/Trees/1361_Validate_Binary_tree_nodes.cpp
@@ -21,6 +21,7 @@
 // Input: n = 6, leftChild = [1,-1,-1,4,-1,-1], rightChild = [2,-1,-1,5,-1,-1]
 // Output: false
 
+// https://leetcode.com/contest/weekly-contest-177/problems/validate-binary-tree-nodes/
 
 class Solution {
     bool helper(vector<bool>& visited, vector<int>& leftChild, vector<int>& rightChild, int n, int& idx)
