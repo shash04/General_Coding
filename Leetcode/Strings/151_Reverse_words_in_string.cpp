ea8c2934321db4bf2be8061d5261c14be341db39
Shashank Phalke <shashankphalke@gatech.edu>
2020-02-27 00:32:02 -0800

    Update 151_Reverse_words_in_string.cpp

--- Leetcode/Strings/151_Reverse_words_in_string.cpp
+++ Leetcode/Strings/151_Reverse_words_in_string.cpp
@@ -17,7 +17,7 @@
 // https://leetcode.com/problems/reverse-words-in-a-string/
 
 
-// Solution 1:
+// Solution 1 - Takes lot of space as compared to solution 2
 class Solution {
 public:
     string reverseWords(string s) {
@@ -31,12 +31,65 @@ public:
                 revString += word;
             else
                 revString = word + " " + revString;
-        }        
+        }
+        
         return revString;
     }
 };
 
-// Solution 2
+// Solution 2 - Space efficient
+class Solution {
+public:
+    string reverseWords(string s) {
+        if(s.size() == 0)
+            return s;
+        
+        string retStr;
+        istringstream ss(s);
+        
+        string currWord;
+        vector<string> wordVec;
+        
+        int i=0;
+        
+        // Remove trailing spaces
+        while(s[i] == ' ' && i < s.size())
+            i++;
+        
+        // Store words in wordVec
+        for(; i < s.size(); i++)
+        {
+            // Check for currWord size
+            if(s[i] == ' ' && currWord.size() != 0)
+            {
+                wordVec.push_back(currWord);
+                currWord.clear();
+                continue;
+            }
+            
+            // Dont add spaces
+            if(s[i] != ' ')
+                currWord += s[i];
+        }
+        
+        // Add if last word was valid
+        if(currWord.size() != 0)
+        {
+            wordVec.push_back(currWord);
+        }
+        
+        for(int i=wordVec.size() - 1; i >= 0; i--)
+        {
+            retStr += wordVec[i] + " ";
+        }
+        
+        retStr = retStr.substr(0, retStr.size() - 1);
+        
+        return retStr;
+    }
+};
+
+// Solution 3
 class Solution {
 public:
     string reverseWords(string s) {
@@ -53,14 +106,19 @@ public:
                     revString += word;
                 else
                     revString = word + " " + revString;
+
                 word = "";
+
                 while(s[i+1] == ' ' && i+1<s.length())
                     i++;
             }
         }
+
         if(word != "" && revString != "")
             word += " ";
+
         revString = word + revString;
+
         return revString;
     }
 };
