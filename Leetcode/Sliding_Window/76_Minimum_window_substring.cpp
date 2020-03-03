7a97ca74b3a245be19c1d5722aaf9a086313aece
Shashank Phalke <shashankphalke@gatech.edu>
2020-03-03 00:58:02 -0800

    Update 76_Minimum_window_substring.cpp

--- Leetcode/Sliding_Window/76_Minimum_window_substring.cpp
+++ Leetcode/Sliding_Window/76_Minimum_window_substring.cpp
@@ -21,13 +21,14 @@ public:
         int counter = t.size();
         
         // Increment indices for chars in t 
-        for( auto i : t )
-            m1[i]++;
+        for(char& c : t)
+            m1[c]++;
         
         while(end < s.size())
         {
             if(m1[s[end]] > 0)              // Char in s exists in t
                 counter--;                  // Decrement the remaining char count to match
+            
             m1[s[end]]--;                   // Decrement index for char. Unmatched char values would be -1.
             end++;                          // Increment end pointer
             
@@ -38,12 +39,19 @@ public:
                     minLen = end - start;   // end already incremented. Thus +1 not required
                     minStart = start;
                 }
+                
                 m1[s[start]]++;             // For sliding window, increment the index which was decremented earlier
+                
                 if(m1[s[start]] > 0)        // If s[start] is present in t, its value now would be 1
                     counter++;              // Sliding past it should increment the remaining char count to match
+                
                 start++;                    // Slide window
             }
         }
-        return minLen == INT_MAX ? "": s.substr(minStart, minLen);
+        
+        if(minLen == INT_MAX)
+            return "";
+        
+        return s.substr(minStart, minLen);
     }
 };
