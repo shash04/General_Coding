5214825539ee56733528ed4055237b95489ca172
Shashank Phalke <shashankphalke@gatech.edu>
2020-03-07 19:38:07 -0800

    Update 253_Meeting_rooms_2.cpp

--- Leetcode/Sorting/253_Meeting_rooms_2.cpp
+++ Leetcode/Sorting/253_Meeting_rooms_2.cpp
@@ -11,13 +11,17 @@
 
 // https://leetcode.com/problems/meeting-rooms-ii/
 
+// **************************************************************************
+// Ordered Map solution
+// **************************************************************************
+
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
         if(!intervals.size())
             return 0;
         
-        map<int, int> m1;
+        map<int, int> m1;                   // map needs to be ordered. Can't use unordered_map
         int roomsRequired = 0;
         int curr_rooms = 0;
         int n = intervals.size();
@@ -34,5 +38,41 @@ public:
             roomsRequired = max(roomsRequired, curr_rooms);
         }
         return roomsRequired;
+    }
+};
+
+
+// **************************************************************************
+// Min heap solution using priority queue - slightly faster
+// **************************************************************************
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        if(intervals.size() == 0)
+            return 0;
+        if(intervals.size() < 2)
+            return 1;
+        
+        int count = 1;
+        
+        // sort intervals according to start times
+        sort(intervals.begin(), intervals.end());
+        
+        // min heap to keep track of lowest end time
+        priority_queue<int, vector<int>, greater<int>> pq;
+        
+        pq.push(intervals[0][1]);
+        
+        for(int i=1; i < intervals.size(); i++)
+        {
+            if(intervals[i][0] < pq.top())              // if start time < current lowest end time = rooms++
+                count++;
+            else
+                pq.pop();                               // start time >= lowest end time = remove lowest end time = re-use room
+            
+            pq.push(intervals[i][1]);                   // Add end time to min heap pq
+        }
+        
+        return count;
     }
 };
