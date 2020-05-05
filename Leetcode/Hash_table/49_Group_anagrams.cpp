e5f093f89e833e05cea4601df5f3421000914338
Shashank Phalke <shashankphalke@gatech.edu>
2020-05-04 20:45:55 -0700

    Update 49_Group_anagrams.cpp

--- Leetcode/Hash_table/49_Group_anagrams.cpp
+++ Leetcode/Hash_table/49_Group_anagrams.cpp
@@ -73,3 +73,37 @@ public:
         return hashKey;
     }
 };
+
+
+// ********************************************************************
+// Alternate method
+// ********************************************************************
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> retVec;
+        
+        map<vector<int>, vector<string>> anagramMap;
+        
+        for(int i=0; i < strs.size(); i++)
+        {
+            vector<int> hashKey = getHashKey(strs[i]);
+            anagramMap[hashKey].push_back(strs[i]);
+        }
+        
+        for(auto& entry : anagramMap)
+            retVec.push_back(entry.second);
+        
+        return retVec;
+    }
+    
+    vector<int> getHashKey(string& s)
+    {
+        vector<int> hashKey (26, 0);
+        
+        for(char& c : s)
+            hashKey[c - 'a']++;
+        
+        return hashKey;
+    }
+};
