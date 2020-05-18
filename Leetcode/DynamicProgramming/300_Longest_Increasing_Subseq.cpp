81b6ff5a225a533f6149fa65c745c3af16b1aba8
Shashank Phalke <shashankphalke@gatech.edu>
2020-05-17 21:26:58 -0700

    300 Longest Increasing Subseq [DP] [MEDIUM]

---
+++ Leetcode/DynamicProgramming/300_Longest_Increasing_Subseq.cpp
@@ -0,0 +1,49 @@
+// Given an unsorted array of integers, find the length of longest increasing subsequence.
+
+// Example:
+// Input: [10,9,2,5,3,7,101,18]
+// Output: 4 
+// Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4. 
+
+// Note:
+// There may be more than one LIS combination, it is only necessary for you to return the length.
+// Your algorithm should run in O(n2) complexity.
+
+// Follow up: Could you improve it to O(n log n) time complexity?
+
+// https://leetcode.com/problems/longest-increasing-subsequence/
+
+// **************************************************************************************************
+// 1D Dp approach - O(N2) time complexity
+// i th index in dp keeps track of LIS till i
+// **************************************************************************************************
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.size() == 0)
+            return 0;
+        
+        // Start with dp and all elements as 1 - min longest subsequence is num itself
+        vector<int> dp (nums.size(), 1);
+        
+        // i would be iterating ahead of j
+        // for e.g. [3,1,2] : for i=1 : j = 0; for i = 2 : j = 0, 1
+        for(int i = 0; i < nums.size(); i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                // If num at j is less than num at i, LIS at j is max(curr value at j, 1 + LIS at i)
+                if(nums[j] < nums[i])
+                    dp[i] = max(dp[i], 1 + dp[j]);
+            }
+        }
+        
+        // max LIS can be at any index in the dp array - iterate to find it
+        int maxLis = 0;
+        
+        for(int i=0; i < nums.size(); i++)
+            maxLis = max(maxLis, dp[i]);
+        
+        return maxLis;
+    }
+};
