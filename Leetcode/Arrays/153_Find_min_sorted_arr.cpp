8e38bb13630f5e2aa0544d40c95d6b090759a668
Shashank Phalke <shashankphalke@gatech.edu>
2020-07-26 16:12:51 -0700

    Find Min Sorted Arr 1 & 2 [ARRAYS] [MEDIUM/HARD]

---
+++ Leetcode/Arrays/153_Find_min_sorted_arr.cpp
@@ -0,0 +1,40 @@
+// Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+// (i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+
+// Find the minimum element.
+
+// You may assume no duplicate exists in the array.
+
+// Example 1:
+// Input: [3,4,5,1,2] 
+// Output: 1
+
+// Example 2:
+// Input: [4,5,6,7,0,1,2]
+// Output: 0
+
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size() - 1;
+        
+        while(start < end)
+        {
+            if(nums[start] < nums[end])
+                return nums[start];
+            
+            int mid = start + (end - start) / 2;
+            
+            if(nums[start] <= nums[mid])
+                start = mid + 1;
+            else
+                end = mid;
+        }
+        
+        return nums[start];
+    }
+};
