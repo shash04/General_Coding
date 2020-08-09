8e08a20359084d0f2c26a2a6f7cfca0c7148aec5
Shashank Phalke <shashankphalke@gatech.edu>
2020-08-08 18:47:33 -0700

    Update 811_subdomain_visit_count.cpp

--- Leetcode/Strings/811_subdomain_visit_count.cpp
+++ Leetcode/Strings/811_subdomain_visit_count.cpp
@@ -4,7 +4,8 @@
 // Output: 
 // ["9001 discuss.leetcode.com", "9001 leetcode.com", "9001 com"]
 // Explanation: 
-// We only have one website domain: "discuss.leetcode.com". As discussed above, the subdomain "leetcode.com" and "com" will also be visited. So they will all be visited 9001 times.
+// We only have one website domain: "discuss.leetcode.com". As discussed above, the subdomain "leetcode.com"
+// and "com" will also be visited. So they will all be visited 9001 times.
 
 // Example 2:
 // Input: 
@@ -13,52 +14,103 @@
 // ["901 mail.com","50 yahoo.com","900 google.mail.com","5 wiki.org","5 org","1 intel.mail.com","951 com"]
 // Explanation: 
 // We will visit "google.mail.com" 900 times, "yahoo.com" 50 times, "intel.mail.com" once and "wiki.org" 5 times.
-// For the subdomains, we will visit "mail.com" 900 + 1 = 901 times, "com" 900 + 50 + 1 = 951 times, and "org" 5 times.
+// we will visit "mail.com" 900 + 1 = 901 times, "com" 900 + 50 + 1 = 951 times, and "org" 5 times.
 
 // https://leetcode.com/problems/subdomain-visit-count/
 
+// *********************************************************************************************
+// Using substr approach - Faster
+// *********************************************************************************************
 class Solution {
-public:
-    void updateDomain(map<string, int> &domainMap, const string &dom, const int &count)
+    unordered_map<string, int> m1;
+    
+    void updateMap(string& s)
     {
-        if(domainMap.find(dom) != domainMap.end())
+        int pos = s.find(' ');
+        int count = stoi(s.substr(0, pos));
+        string domain = s.substr(pos + 1);
+        
+        m1[domain] += count;
+        
+        pos = domain.find('.');
+        
+        while(pos != string::npos)
         {
-            domainMap[dom] += count;
-        }
-        else
-        {
-            domainMap[dom] = count;
+            domain = domain.substr(pos + 1);
+            m1[domain] += count;
+            pos = domain.find('.');
         }
     }
-    
+public:
     vector<string> subdomainVisits(vector<string>& cpdomains) {
-        map<string, int> domainMap;
         vector<string> retVec;
-        for(int i=0; i<cpdomains.size(); i++)
+        
+        for(string& s : cpdomains)
         {
-            string entry = cpdomains[i];
-            
-            int pos_space = entry.find(" ");
-            
-            string countStr = entry.substr(0, pos_space);
-            int count = stoi(countStr);
-            
-            string domain = entry.substr(pos_space+1);
-            updateDomain(domainMap, domain, count);
-            
-            int pos_dot = domain.find(".");
-            while(pos_dot != string::npos)
-            {
-                domain = domain.substr(pos_dot+1);
-                updateDomain(domainMap, domain, count);
-                pos_dot = domain.find(".");
-            }
+            updateMap(s);
         }
         
-        for(auto iter=domainMap.begin(); iter != domainMap.end(); iter++)
+        for(auto& entry : m1)
         {
-            string str = to_string(iter->second) + " " + iter->first;
-            retVec.push_back(str);
+            string tmp = to_string(entry.second) + " " + entry.first;
+            retVec.push_back(tmp);
+        }
+        
+        return retVec;
+    }
+};
+
+// *********************************************************************************************
+// Using stringstream approach
+// *********************************************************************************************
+class Solution {
+    unordered_map<string, int> m1;
+    
+    void updateMap(string& s)
+    {
+        stringstream ss(s);
+        string currStr;
+        vector<string> v1;
+        
+        while(ss >> currStr)
+        {
+            stringstream split(currStr);
+            string word;
+            
+            if(v1.empty())
+            {
+                v1.push_back(currStr);
+                continue;
+            }
+            
+            while(getline(split, word, '.'))
+            {
+                v1.push_back(word);
+            }
+            
+            string tmp;
+            
+            for(int i = v1.size() - 1; i >= 1; i--)
+            {
+                tmp = v1[i] + tmp;
+                m1[tmp] += stoi(v1[0]);
+                tmp = '.' + tmp; 
+            }
+        }
+    }
+public:
+    vector<string> subdomainVisits(vector<string>& cpdomains) {
+        vector<string> retVec;
+        
+        for(string& s : cpdomains)
+        {
+            updateMap(s);
+        }
+        
+        for(auto& entry : m1)
+        {
+            string tmp = to_string(entry.second) + " " + entry.first;
+            retVec.push_back(tmp);
         }
         
         return retVec;
