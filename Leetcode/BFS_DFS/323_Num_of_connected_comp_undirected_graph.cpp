a11ef3d8817c3b4b79561c30e388362c1a151921
Shashank Phalke <shashankphalke@gatech.edu>
2020-09-20 17:23:10 -0700

    323 Num Of connected comp in undir graph [DFS] [MEDIUM]

---
+++ Leetcode/BFS_DFS/323_Num_of_connected_comp_undirected_graph.cpp
@@ -0,0 +1,72 @@
+// Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes),
+// write a function to find the number of connected components in an undirected graph.
+
+// Example 1:
+// Input: n = 5 and edges = [[0, 1], [1, 2], [3, 4]]
+
+//      0          3
+//      |          |
+//      1 --- 2    4 
+
+// Output: 2
+
+// Example 2:
+// Input: n = 5 and edges = [[0, 1], [1, 2], [2, 3], [3, 4]]
+
+//      0           4
+//      |           |
+//      1 --- 2 --- 3
+
+// Output:  1
+// Note:
+// You can assume that no duplicate edges will appear in edges.
+// Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
+
+// https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
+
+// **************************************************************************************************************
+// Concept = Populate a graph and try to visit all nodes
+// DFS - connected components will be visited in one call
+// number of times DFS called = num of components in undirected graph
+// **************************************************************************************************************
+class Solution {
+    unordered_map<int, vector<int>> graph;
+    
+    void DFS(int node, int depth, vector<bool>& visited)
+    {        
+        visited[node] = true;
+        
+        for(int& next : graph[node])
+        {
+            if(visited[next])
+                continue;
+            
+            DFS(next, depth + 1, visited);
+        }
+    }
+    
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {        
+        int result = 0;
+        
+        vector<bool> visited (n, false);
+        
+        for(auto& edge : edges)
+        {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(visited[i])
+                continue;
+            
+            result++;
+    
+            DFS(i, 0, visited);
+        }
+        
+        return result;
+    }
+};
